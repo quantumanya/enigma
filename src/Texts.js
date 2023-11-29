dd8b44c097bde211e88f69bb8789a9768904c9ed
Anya Petrova <petrovaacnts@gmail.com>
2023-11-29 11:12:54 -0800

    bring back dimContent

---
+++ src/Texts.js
@@ -0,0 +1,11 @@
+<p>
+                "Final Enigma" is a podcast series exploring the mysteries and the unknowns within and on the intersections of 
+                Physics, Neuroscience and AI with the ultimate goal of probing into the depths of the most important final enigma of science: human cosciousness. 
+                <br />
+                <br />I strongly believe that scientific research into the nature, structure and phenomenology of cosciousness is of the utmost urgency and umportance if humanity is to invent AGI safely. 
+
+                <br /> 
+                <br />
+                Join me on this adventure across the most fascinating and enigmatic mysteries of science as I interview key scientists and researchers, probing their minds and ultimatelly, the great unknown.  
+
+                </p>
--- src/Vienn.js
+++ src/Vienn.js
@@ -1,42 +1,15 @@
-import React, { useState } from 'react';
-import Circle from './Circle';
-import Eye from './Eye';
-import ActiveContent from './ActiveContent';
-import ThreeEye from './ThreeEye';
-import {CircleSmall, CircleCenter} from './Circle';
-// import {CircleSmall} from './Circle';
-import './Styles.css'; // Import the CSS file here
+function Vienn({}) {
 
-function Circles() {
+    const [activeCircle, setActiveCircle] = useState(null);
+    const [showEyeSet1, setShowEyeSet1] = useState(false); // State for Eye of set 1
+    const [showEyeSet2, setShowEyeSet2] = useState(false); // State for Eye of set 2
+    const [showEyeSet3, setShowEyeSet3] = useState(false); // State for Eye of set 3
+    const [showThreeEye, setShowThreeEye] = useState(false); // State for Eye of set 3
 
-  const [activeCircle, setActiveCircle] = useState(null);
-  const [showEyeSet1, setShowEyeSet1] = useState(false); // State for Eye of set 1
-  const [showEyeSet2, setShowEyeSet2] = useState(false); // State for Eye of set 2
-  const [showEyeSet3, setShowEyeSet3] = useState(false); // State for Eye of set 3
-  const [showThreeEye, setShowThreeEye] = useState(false); // State for Eye of set 3
 
-  const [dimContent, setDimContent] = useState(false); // State for Eye of set 3
-  const [showHiddenLayer, setShowHiddenLayer] = useState(false); // State for the hidden layer
-  const [activeContent, setActiveContent] = useState(null); // New state for active content
-
-  const [text, setText] = useState('Default Text');
-
-  const contentDictionary = {
-        what: 'Whatttt',
-        when: 'wheeeen',
-        who: 'whoooo',
-        why: 'whyyy'
-    }
-
-  // Function to change the text
-  const changeText = (newText) => {
-    setText(contentDictionary[newText]);
-  };
+    const [showHiddenLayer, setShowHiddenLayer] = useState(false); // State for the hidden layer
 
   return (
-<div className="p-2 flex items-center justify-center bg-black relative">
-
-
 
     <div className="relative w-96 h-96 flex items-center justify-center bg-black">
       
@@ -157,30 +130,5 @@ function Circles() {
 
 
     </div>
-
-    {/* INFO MENU ITEMS */}
-      <div className='leftLeftContentDivStyle' style={{ opacity: dimContent ? 0.3 : 1 }}>
-              <a href="#" className='buttonStyle'  onClick={() => changeText('what')}>What</a><br/>
-              <a href="#" className='buttonStyle'  onClick={() => changeText('when')}>When</a><br/>
-              <a href="#" className='buttonStyle'  onClick={() => changeText('who')}>Who</a><br/>
-              <a href="#" className='buttonStyle'  onClick={() => changeText('why')}>Why</a><br/>
-              <a href="#" className='buttonStyle'  onClick={() => changeText('why')}>Where</a><br/>
-              <a href="#" className='buttonStyle'  onClick={() => changeText('why')}>Whom</a><br/>
-       </div>
-    {/* Content Div: LEFT */}
-      <div className='leftContentDivStyle' style={{ opacity: dimContent ? 0.3 : 1 }}>
-              {/* Div displaying the text */}
-              <div>{text}</div>  
-      </div>
-
-    {/* Content Div: RIGHT */}
-      <div className='contentDivStyle'>
-        {/* Display content based on activeContent state */}
-        {activeContent && <ActiveContent currentState={activeContent} />}
-      </div> 
-    </div>
-  );
-};
-
-
-export default Circles;
+  )
+}
