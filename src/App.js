6d927da7f3acb1667f9be5600aa42dc2f733fa7d
Anya Petrova <petrovaacnts@gmail.com>
2023-11-29 10:10:58 -0800

    App.js changes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,22 @@ const App = () => {
   return (
     <>
 
+      <div style={{ textAlign: 'center' }}>
+          <div style={{ 
+            display: 'inline-block', 
+            width: '860pt',
+            textAlign: 'left',
+            fontFamily: 'monospace', 
+            fontWeight: '400', 
+            color: '#bdbdbd', 
+            fontSize: '14pt',
+            paddingTop: '14pt',
+            }}>
 
-      <div className="p-2 flex items-center justify-left bg-black relative"
-        style={{
-          paddingLeft: '80pt',
-          paddingTop: '16pt',
-        }}
-        >
-        <span style={{
-          fontFamily: 'monospace',
-          fontWeight: '400',
-          }}>
-          <span style={{color: '#bdbdbd', fontSize: '14pt'}}>FINAL</span>
-          <span style={{color: '#bdbdbd', fontSize: '14pt'}}>ENIGMA</span>
-          {/* <span style={{color: '#bdbdbd', fontSize: '10pt'}}>[PODCAST]</span>   */}
-          {/* <span style={{color: '#525252', fontSize: '10pt'}}> Exploring a Scientific Quest to Unlock Human and Machine Consciousness</span> */}
-            
-        </span>
-      </div>
+           Final Enigma 
+
+          </div>
+        </div>
 
       <div>
         <div className="p-2 flex items-center justify-center bg-black relative"
